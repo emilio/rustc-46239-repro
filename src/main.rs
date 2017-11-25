0476e066016b355a4ceebb591d3066a47956b6ff
Emilio Cobos Álvarez <emilio@crisal.io>
2017-11-25 02:42:14 +0100

    Inline a fair bit of the link! macro.

--- src/main.rs
+++ src/main.rs
@@ -181,21 +181,6 @@ fn parse_search_paths(path: &Path, language: &str, args: &[String]) -> Option<Ve
 }
 
 macro_rules! link {
-    (@LOAD: #[cfg($cfg:meta)] fn $name:ident($($pname:ident: $pty:ty), *) $(-> $ret:ty)*) => (
-        #[cfg($cfg)]
-        pub fn $name(library: &mut super::SharedLibrary) {
-            let symbol = unsafe { library.library.get(stringify!($name).as_bytes()) }.ok();
-            library.functions.$name = symbol.map(|s| *s);
-        }
-
-        #[cfg(not($cfg))]
-        pub fn $name(_: &mut super::SharedLibrary) {}
-    );
-
-    (@LOAD: fn $name:ident($($pname:ident: $pty:ty), *) $(-> $ret:ty)*) => (
-        link!(@LOAD: #[cfg(any(feature="runtime", not(feature="runtime")))] fn $name($($pname: $pty), *) $(-> $ret)*);
-    );
-
     ($($(#[cfg($cfg:meta)])* pub fn $name:ident($($pname:ident: $pty:ty), *) $(-> $ret:ty)*;)+) => (
         use std::cell::{RefCell};
         use std::sync::{Arc};
@@ -256,76 +241,33 @@ macro_rules! link {
                 }
             }
         )+
-
-        mod load {
-            $(link!(@LOAD: $(#[cfg($cfg)])* fn $name($($pname: $pty), *) $(-> $ret)*);)+
-        }
-
-        /// Loads a `libclang` shared library and returns the library instance.
-        ///
-        /// This function does not attempt to load any functions from the shared library. The caller
-        /// is responsible for loading the functions they require.
-        ///
-        /// # Failures
-        ///
-        /// * a `libclang` shared library could not be found
-        /// * the `libclang` shared library could not be opened
-        pub fn load_manually() -> Result<SharedLibrary, String> {
-            let file = try!(build::find_shared_library());
-            let library = libloading::Library::new(&file).map_err(|_| {
-                format!("the `libclang` shared library could not be opened: {}", file.display())
-            });
-            let mut library = SharedLibrary::new(try!(library));
-            $(load::$name(&mut library);)+
-            Ok(library)
-        }
-
-        /// Loads a `libclang` shared library for use in the current thread.
-        ///
-        /// This functions attempts to load all the functions in the shared library. Whether a
-        /// function has been loaded can be tested by calling the `is_loaded` function on the
-        /// module with the same name as the function (e.g., `clang_createIndex::is_loaded()` for
-        /// the `clang_createIndex` function).
-        ///
-        /// # Failures
-        ///
-        /// * a `libclang` shared library could not be found
-        /// * the `libclang` shared library could not be opened
-        #[allow(dead_code)]
-        pub fn load() -> Result<(), String> {
-            let library = Arc::new(try!(load_manually()));
-            LIBRARY.with(|l| *l.borrow_mut() = Some(library));
-            Ok(())
-        }
-
-        /// Unloads the `libclang` shared library in use in the current thread.
-        ///
-        /// # Failures
-        ///
-        /// * a `libclang` shared library is not in use in the current thread
-        pub fn unload() -> Result<(), String> {
-            let library = set_library(None);
-            if library.is_some() {
-                Ok(())
-            } else {
-                Err("a `libclang` shared library is not in use in the current thread".into())
-            }
-        }
-
-        /// Returns the library instance stored in TLS.
-        ///
-        /// This functions allows for sharing library instances between threads.
-        pub fn get_library() -> Option<Arc<SharedLibrary>> {
-            LIBRARY.with(|l| l.borrow_mut().clone())
-        }
-
-        /// Sets the library instance stored in TLS and returns the previous library.
-        ///
-        /// This functions allows for sharing library instances between threads.
-        pub fn set_library(library: Option<Arc<SharedLibrary>>) -> Option<Arc<SharedLibrary>> {
-            LIBRARY.with(|l| mem::replace(&mut *l.borrow_mut(), library))
-        }
     )
+}
+
+mod load {
+    pub fn clang_createIndex(library: &mut super::SharedLibrary) {
+        let symbol = unsafe { library.library.get(b"clang_createIndex") }.ok();
+        library.functions.clang_createIndex = symbol.map(|s| *s);
+    }
+}
+
+/// Loads a `libclang` shared library and returns the library instance.
+///
+/// This function does not attempt to load any functions from the shared library. The caller
+/// is responsible for loading the functions they require.
+///
+/// # Failures
+///
+/// * a `libclang` shared library could not be found
+/// * the `libclang` shared library could not be opened
+pub fn load_manually() -> Result<SharedLibrary, String> {
+    let file = try!(build::find_shared_library());
+    let library = libloading::Library::new(&file).map_err(|_| {
+        format!("the `libclang` shared library could not be opened: {}", file.display())
+    });
+    let mut library = SharedLibrary::new(try!(library));
+    load::clang_createIndex(&mut library);
+    Ok(library)
 }
 
 use std::mem;
@@ -628,9 +570,8 @@ fn get_clang_libraries<P: AsRef<Path>>(directory: P) -> Vec<String> {
 }
 
 fn main() {
-    if !is_loaded() {
-        load().unwrap();
-    }
-    unsafe { clang_createIndex(0, 1) };
+    let lib = load_manually().unwrap();
+    let fun = lib.functions.clang_createIndex.unwrap();
+    unsafe { fun(0, 1) };
     println!("Did I survive?");
 }
